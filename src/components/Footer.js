47d51ed186f235b110f39ab9bb153676f88025e3
bugman <onlyforwin@gmail.com>
2019-04-19 10:15:41 +0200

    FIX: add addictive data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,15 +29,52 @@ const Footer = class extends React.Component {
                         Home
                       </Link>
                     </li>
+                    <li>
+                      <a className="navbar-item" href="mailto:financial@pitchtarget.com">
+                        Contact
+                      </a>
+                    </li>
                   </ul>
                 </section>
+              </div>
+              <div className="column is-4 social">
+                <a title="facebook" href="https://facebook.com">
+                  <img
+                    src={facebook}
+                    alt="Facebook"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="twitter" href="https://twitter.com">
+                  <img
+                    className="fas fa-lg"
+                    src={twitter}
+                    alt="Twitter"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="instagram" href="https://instagram.com">
+                  <img
+                    src={instagram}
+                    alt="Instagram"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="vimeo" href="https://vimeo.com">
+                  <img
+                    src={vimeo}
+                    alt="Vimeo"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
               </div>
               <div className="column is-4">
                 <section>
                   <ul className="menu-list">
-                    <li>
-                      <a className="navbar-item" href="mailto:financial@pitchtarget.com">Contact</a>
-                    </li>
+                    <li>Addictive s.r.l</li>
+                    <li>Via Sile 41, 31056, Roncade (TV), Italia</li>
+                    <li>Phone: +390422789611</li>
+                    <li>VAT: IT04636600266</li>
                   </ul>
                 </section>
               </div>
